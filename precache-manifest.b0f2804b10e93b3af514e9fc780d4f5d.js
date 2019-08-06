396cef81bf5bcd97195f166bd76f78f58a78dc42
apisit Amunayworrabut <minrrverse@gmail.com>
2019-08-06 14:26:05 +0700

    INIT

---
+++ precache-manifest.b0f2804b10e93b3af514e9fc780d4f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a6ff9d17f91769606d214cf785aee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1289c2c358e6a72ed29d",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "4babbcd2fda08577f60d",
+    "url": "/static/css/main.7fd796c2.chunk.css"
+  },
+  {
+    "revision": "1289c2c358e6a72ed29d",
+    "url": "/static/js/2.369e64fb.chunk.js"
+  },
+  {
+    "revision": "4babbcd2fda08577f60d",
+    "url": "/static/js/main.4203d7f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
